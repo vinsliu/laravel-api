8485876ac96e6c6fa967cb94c4ed73b190104589
Vincent <vincent00lav@outlook.com>
2026-02-09 11:29:32 +0100

    Activité 1.2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::middleware('api')->prefix('api/v1');
+            Route::middleware('api')->prefix('api/v1')->group(base_path('routes/api.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
