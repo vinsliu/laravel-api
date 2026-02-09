bdf06860362b90ff965647d6a2fd7294f6114392
Vincent <vincent00lav@outlook.com>
2026-02-09 10:54:55 +0100

    Activité 1.1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
-        apiPrefix: 'api/v1',    
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')->prefix('api/v1');
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173'],
     'allowed_headers' => ['*'],
